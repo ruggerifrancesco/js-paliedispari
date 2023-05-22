e76900a8650234aa7e2a21d00c67b00441037dba
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-22 17:01:01 +0200

    invoked random number between 1 and 5

--- even-odd/script.js
+++ even-odd/script.js
@@ -11,9 +11,12 @@ Dichiariamo chi ha vinto.
 const isEvenOrOdd = prompt("Scegli tra Pari o Dispari");
 const userNumber = parseInt(prompt("Scegli un numero da 1 a 5!"));
 
-// Functions
+// ----> Functions
 
 // Random Number
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+    // Invoke getRndInteger
+    getRndInteger(1, 5)
