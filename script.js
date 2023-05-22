9eae5a39c5872fd372e94b2d29175a1c57cc73a4
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-22 16:18:28 +0200

    Defined prompt and first fucntion content

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('JS Loaded Successfully!');
+
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+const userInput = prompt('Entra una parrola e scopri se e palindroma!');
+
+function ifWordPalindrome(str) {
+    const wordLength = str.length;
+}
