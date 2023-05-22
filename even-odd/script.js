3ac028acf41d8df30b351061d2c6e7b649b14e45
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-22 16:56:35 +0200

    added first commnets

--- even-odd/script.js
+++ even-odd/script.js
@@ -12,6 +12,8 @@ const isEvenOrOdd = prompt("Scegli tra Pari o Dispari");
 const userNumber = parseInt(prompt("Scegli un numero da 1 a 5!"));
 
 // Functions
+
+// Random Number
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
