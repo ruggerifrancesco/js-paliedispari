0d251e0c1630c1b5474d84577ea4876aa9d74ac6
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-22 16:53:24 +0200

    added pormpots user and function random number

--- even-odd/script.js
+++ even-odd/script.js
@@ -7,3 +7,11 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+const isEvenOrOdd = prompt("Scegli tra Pari o Dispari");
+const userNumber = parseInt(prompt("Scegli un numero da 1 a 5!"));
+
+// Functions
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
